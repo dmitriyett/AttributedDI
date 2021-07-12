ccd4b102690f2364b463cf3e0d368cee3a696250
Dmitriy Ett <dmitriy.ett@hotmail.com>
2021-07-12 20:19:04 +0300

    finished DI extensions

--- src/AttributedDI/RegistrationServiceCollectionExtensions.cs
+++ src/AttributedDI/RegistrationServiceCollectionExtensions.cs
@@ -1,29 +1,50 @@
 using System;
+using System.Linq;
 using System.Reflection;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace AttributedDI
 {
+    // TODO: make it testable.
     public static class RegistrationServiceCollectionExtensions
     {
-        public static IServiceCollection AddServicesFromExecutingAssembly(this IServiceCollection services)
-        {
-
-        }
-
-        public static IServiceCollection AddServicesFromAssembly(this IServiceCollection services, Assembly assembly)
-        {
-
-        }
-
         public static IServiceCollection AddServicesFromAssemblyContainingType<T>(this IServiceCollection services)
         {
-
+            return AddServicesFromAssembly(services, typeof(T).Assembly);
         }
 
         public static IServiceCollection AddServicesFromAssemblyContainingType(this IServiceCollection services, Type type)
         {
+            return AddServicesFromAssembly(services, type.Assembly);
+        }
 
+        public static IServiceCollection AddServicesFromAssembly(this IServiceCollection services, Assembly assembly)
+        {
+            var typesToRegister = assembly.GetTypes().Where(ContainsRegisterAttribute).ToArray();
+
+            foreach (var type in typesToRegister)
+            {
+                PerformRegistration(services, type);
+            }
+
+            return services;
+        }
+
+        private static bool ContainsRegisterAttribute(Type type)
+        {
+            var registerBaseType = typeof(RegisterBase);
+
+            return type.CustomAttributes.Any(a => typeof(RegisterBase).IsAssignableFrom(a.AttributeType));
+        }
+
+        private static void PerformRegistration(IServiceCollection services, Type type)
+        {
+            var attributes = type.GetCustomAttributes<RegisterBase>();
+
+            foreach (var attribute in attributes)
+            {
+                attribute.PerformRegistration(services, type);
+            }
         }
     }
 }
