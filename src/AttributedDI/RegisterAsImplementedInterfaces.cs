97f56def9c4233f56e97887d0e64384b4cddc7dd
Dmitriy Ett <dmitriy.ett@hotmail.com>
2021-07-12 19:14:03 +0300

    Register as implemented interfaces

---
+++ src/AttributedDI/RegisterAsImplementedInterfaces.cs
@@ -0,0 +1,29 @@
+using System;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AttributedDI
+{
+    public class RegisterAsImplementedInterfaces : RegisterBase
+    {
+        public RegisterAsImplementedInterfaces(Lifetime lifetime) : base(lifetime)
+        {
+        }
+
+        public override void PerformRegistration(IServiceCollection services, Type target)
+        {
+            var interfaces = target.GetInterfaces();
+
+            if (interfaces.Length == 0)
+            {
+                throw new ArgumentException("Type should implement at least one interface to be registered.", nameof(target));
+            }
+
+            foreach (var @interface in interfaces)
+            {
+                var descriptor = CreateDescriptor(@interface, target);
+
+                services.Add(descriptor);
+            }
+        }
+    }
+}
