c3678519b3d46105c9f5adb9562608822ba462dd
Dmitriy Ett <dmitriy.ett@hotmail.com>
2021-07-15 17:08:15 +0300

    editorconfig

--- src/AttributedDI/AssemblyScanner.cs
+++ src/AttributedDI/AssemblyScanner.cs
@@ -5,12 +5,25 @@ using System.Reflection;
 
 namespace AttributedDI
 {
+    /// <summary>
+    /// Assembly scan result.
+    /// </summary>
+    /// <param name="Service">Service to register</param>
+    /// <param name="RegisterAttribute">Attached registration attribute</param>
     public record AssemblyScanResult(Type Service, RegisterBase RegisterAttribute);
 
+    /// <summary>
+    /// Scans an assembly for types that need to be registered in <see cref="Microsoft.Extensions.DependencyInjection.IServiceCollection"/>.
+    /// </summary>
     public class AssemblyScanner
     {
         private static readonly Type RegisterBaseType = typeof(RegisterBase);
 
+        /// <summary>
+        /// Scan an assembly for types to be registered.
+        /// </summary>
+        /// <param name="assembly">Assembly to be scanned</param>
+        /// <returns>List of types to be registered.</returns>
         public AssemblyScanResult[] Scan(Assembly assembly)
         {
             var results = assembly.GetTypes()
